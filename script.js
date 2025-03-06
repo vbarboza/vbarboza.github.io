b9973173afdaa3a49c697c0245475ac9775d8ddb
Vinicius Barboza <565283+vbarboza@users.noreply.github.com>
2025-03-05 23:50:34 -0300

    Footer script

--- script.js
+++ script.js
@@ -14,10 +14,22 @@ document.addEventListener('DOMContentLoaded', function() {
     if (taglineElement) {
       const randomIndex = Math.floor(Math.random() * taglines.length);
       taglineElement.textContent = taglines[randomIndex];
+    }
 
-      taglineElement.addEventListener('click', function() {
-        const newRandomIndex = Math.floor(Math.random() * taglines.length);
-        taglineElement.textContent = taglines[newRandomIndex];
-      });
+    const footerTexts = [
+      "En barrio de ricachones, sin armas ni rencores, es sólo plata y no amores.",
+      "All your base are belong to us.",
+      "The cake is a lie.",
+      "Foo was here.",
+      "I'm a teapot.",
+      "No cookie for you.",
+      "(fnord)",
+    ]
+
+    const footerTextElement = document.getElementById('footer');
+
+    if (footerTextElement) {
+      const randomIndex = Math.floor(Math.random() * footerTexts.length);
+      footerTextElement.textContent = footerTexts[randomIndex];
     }
   });
