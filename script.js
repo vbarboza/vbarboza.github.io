46030bd194a4f8af587f288090af3c7ad00ecfa9
Vinicius Barboza <565283+vbarboza@users.noreply.github.com>
2025-04-28 23:36:11 -0300

    More updates

--- script.js
+++ script.js
@@ -17,13 +17,8 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     const footerTexts = [
-      "En barrio de ricachones, sin armas ni rencores, es sólo plata y no amores.",
-      "All your base are belong to us.",
-      "The cake is a lie.",
-      "Foo was here.",
-      "I'm a teapot.",
-      "No cookie for you.",
-      "(fnord)",
+
+      "\"You will say that I am always conjuring up awful difficulties & consequences - my answer to this is it is an important part of the duty of an engineer.\" (Robert Stephenson)"
     ]
 
     const footerTextElement = document.getElementById('footer');
